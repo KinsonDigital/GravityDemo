2e03c13e4bcea3b67a65452de8ca766b3d64dbd8
KinsonDigital <kinsondigital@gmail.com>
2018-04-17 15:39:16 -0400

    Extract the calculation of the air/fluid drag on an object to a method

--- GravityTesting/Util.cs
+++ GravityTesting/Util.cs
@@ -13,10 +13,12 @@ namespace GravityTesting
             return mass * acceleration;
         }
 
+
         public static float Squared(float value)
         {
             return value * value;
         }
+
 
         public static float Average(float[] values)
         {
@@ -30,16 +32,38 @@ namespace GravityTesting
             return sum / values.Length;
         }
 
+
         /// <summary>
         /// This performs a verlet velocity integration on a single axis.
         /// </summary>
         /// <param name="velOnSingleAxis">The velocity on a single axis.  Must be the same axis as the <paramref name="accelerationOnSingleAxis"/> param.</param>
         /// <param name="dt">The delta time in seconds of the current frame.</param>
         /// <param name="accelerationOnSingleAxis">The current accerlation on a single axis.  Must be the same axis as the <paramref name="velOnSingleAxis"/> param.</param>
+        /// <remarks>Refer to links for more information.
+        /// Videos:
+        ///     Part 1: https://www.youtube.com/watch?v=3HjO_RGIjCU
+        ///     Part 2: https://www.youtube.com/watch?v=pBMivz4rIJY
+        /// Other: https://leios.gitbooks.io/algorithm-archive/content/chapters/physics_solvers/verlet/verlet.html
+        /// </remarks>
         /// <returns></returns>
         public static float IntegrateVelocityVerlet(float velOnSingleAxis, float dt, float accelerationOnSingleAxis)
         {
             return velOnSingleAxis * dt + (0.5f * accelerationOnSingleAxis * Squared(dt));
+        }
+
+
+        /// <summary>
+        /// Calculates the drag force of air/fluid on the surface of an object.
+        /// </summary>
+        /// <param name="fluidDensity"></param>
+        /// <param name="dragCoefficient"></param>
+        /// <param name="surfaceAreaInContact"></param>
+        /// <param name="velocityOnSingleAxis"></param>
+        /// <remarks>Refer to http://www.softschools.com/formulas/physics/air_resistance_formula/85/ for information.</remarks>
+        /// <returns></returns>
+        public static float CalculateDragForceOnObject(float fluidDensity, float dragCoefficient, float surfaceAreaInContact, float velocityOnSingleAxis)
+        {
+            return -1 * ((fluidDensity * dragCoefficient * surfaceAreaInContact) / 2.0f) * Squared(velocityOnSingleAxis);
         }
     }
 }
